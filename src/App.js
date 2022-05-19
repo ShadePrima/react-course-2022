b113458175e0d9ea024167c88971328e6ff35ec6
ShadePrima <aleksandrag1984@gmail.com>
2022-05-19 15:46:44 +0300

    meneged component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState(1)
-  
-  function increment () {
+  const [value, setValue] = useState("Text in input")
+
+  function increment() {
     setLikes(likes + 1)
   }
 
-  function decrement () {
+  function decrement() {
     setLikes(likes - 1)
   }
-  
+
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input  
+        type="text" 
+        value={value} 
+        onChange={event => setValue(event.target.value)  }
+      />
       <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>      
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
