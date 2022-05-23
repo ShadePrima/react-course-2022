c60585a68519d7dfce206edc7a654581853b3757
ShadePrima <aleksandrag1984@gmail.com>
2022-05-23 16:35:10 +0300

    myInput

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ClassCounter from './components/Counter/ClassCounter';
 import Counter from './components/Counter/Counter';
 import PostItem from './components/PostItem/PostItem';
 import PostList from './components/PostList/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css'
 
 function App() {
@@ -13,18 +15,28 @@ function App() {
 
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: "Python", body: "Description" },
-    { id: 2, title: "Python2", body: "Description" },
-    { id: 3, title: "Python3", body: "Description" },
+  const [title, setTitle] = useState("example")
 
-  ])
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+  }
+ 
 
 
   return (
     <div className="App">
+      <form>
+        <MyInput
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        type="text" 
+        placeholder="Name post"      
+        />
+        <MyInput type="text" placeholder="Post description"/>
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
       <PostList posts={posts} title="Posts of JS"/>
-      <PostList posts={posts2} title="Posts of Python"/>
     </div>
   );
 }
