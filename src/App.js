6223d590dc673d898cc4518f3261a50eb6c2b720
ShadePrima <aleksandrag1984@gmail.com>
2022-05-25 18:01:31 +0300

    css for mySelect

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ function App() {
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts () {
+    console.log('finished function')
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts
+  }
+
+  const sortedPosts = getSortedPosts()
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -28,14 +39,20 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
  
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: "15px 0"}}/>
+
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Search ..."
+        />
+
         <MySelect
           value={selectedSort}
           onChange={sortPosts}
@@ -45,10 +62,11 @@ function App() {
             {value: "body", name: "By discription"},
           ]} 
         />
+
       </div>
       {posts.length !== 0
         ? 
-        <PostList remove={removePost} posts={posts} title="Posts of JS"/>
+        <PostList remove={removePost} posts={sortedPosts} title="Posts of JS"/>
         : 
         <h1 style={{textAlign: "center", color: "teal"}}>
           No posts found
