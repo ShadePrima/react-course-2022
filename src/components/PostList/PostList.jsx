dd729a84e7d0f7d0811269a584ba6069b7c3caf3
ShadePrima <aleksandrag1984@gmail.com>
2022-05-26 15:14:20 +0300

    animation post item

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import { TransitionGroup } from 'react-transition-group'
 import PostItem from '../PostItem/PostItem'
 import classes from './PostList.module.css'
 
-const PostList = ({posts, title, remove}) => {
+const PostList = ({ posts, title, remove }) => {
     if (!posts.length) {
         return (
-        <h1 style={{textAlign: "center", color: "teal", marginTop: "10px"}}>
-            No posts found
-      </h1>
-    )
+            <h1 style={{ textAlign: "center", color: "teal", marginTop: "10px" }}>
+                No posts found
+            </h1>
+        )
     }
     return (
         <div>
             <h1 className={classes.myTitle}>
                 {title}
             </h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     )
 }
